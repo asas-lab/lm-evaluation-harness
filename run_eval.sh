b8eb6c4ad61c9af5254167bc2e9c9b9d635be6d1
KhalidAlt <ekh@Protonmail.com>
2023-11-05 16:44:19 +0300

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,12 +1,12 @@
 output_path="/home/khalid/Documents/github_rep/evalharness/lm_results/"
 Q_MODEL="/media/khalid/HDD2/Huggingface_models/8bit/bloom_560m_8bit"
-BaseModel="/media/khalid/HDD2/Huggingface_models/bloom_7b"
-qlora="asas-ai/bloom_360M_4bit_qlora_mlqa"
-peft=""
+BaseModel="/home/khalid/Documents/github_rep/rwkv/RWKV_LORA/HF-For-RWKVWorld-LoraAlpaca/output_model/rwkv-4-world-7b-arabic"
+qlora="asas-ai/bloom_560m_8bit_qlora_flores"
+
 
 CUDA_VISIBLE_DEVICES=1 python main.py  \
     --model hf-causal-experimental \
-    --model_args pretrained=$Q_MODEL\
-    --tasks xnli_ar \
+    --model_args pretrained=$BaseModel\
+    --tasks ajgt_tw_ar \
     --num_fewshot 0 \
     --batch_size 1 \
